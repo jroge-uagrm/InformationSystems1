6a45659c48a9b89768d54f73c991f70fb42691ce
Jroge <jorgerodrigotorrez@gmail.com>
2019-04-16 23:47:44 -0400

    clear

---
+++ Triggers.sql
@@ -0,0 +1,20 @@
+﻿/*1	Validar que la persona que realiza 
+	la venta pertenece al departamento de ventas*/
+create trigger validacionVenta
+on notaDeVenta 
+for insert
+as
+if ((
+	select departamento.nombre
+	from personalAdministrativo,cargo,departamento
+	where inserted.codigoPersonalAdministrativo=personalAdministrativo.codigo and
+			personalAdministrativo.idCargo=cargo.id and
+			cargo.idDepartamento=departamento.id)!='Departamento de ventas')
+begin
+	print 'El trabajador no pertenece al depto. de ventas.'
+	ROLLBACK TRANSACTION
+end
+
+/*1	Aumentar la cantidad de cargos en un departamento
+	al ingresar un nuevo cargo*/
+
