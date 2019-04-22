e1d2f46d7e5b39d120c1bb182cc5dcc01b22763d
Jroge <jorgerodrigotorrez@gmail.com>
2019-04-21 22:08:25 -0400

    Procedimientos almacenados iniciados

--- ProcedimientosAlmacenados.sql
+++ ProcedimientosAlmacenados.sql
@@ -18,10 +18,72 @@ as begin
 	return @cuposDisponibles
 end
 
-/*2 Obtener la cantidad que falta por pagar
+/*2 Obtener el monto que falta por pagar
 	de una nota de venta*/
-create function
+	use SIinegas;
+create function montoPorPagar(@nroNotaDeVenta int)returns float
+as begin
+	declare @cantidadDeCuotasNoPagadas int
+	set @cantidadDeCuotasNoPagadas=(
+		select count(*)
+		from cuota
+		where cuota.nroNotaDeVenta=@nroNotaDeVenta and
+			cuota.estado=0)
+	declare @montoFaltante float
+	set @montoFaltante=@cantidadDeCuotasNoPagadas*(
+		select monto
+		from cuota 
+		where nroNotaDeVenta=@nroNotaDeVenta)
+	return @montoFaltante
+end
 
+/*3 Obtener la cantidad de cuotas que falta 
+	por pagar de una nota de venta*/
+	use SIinegas;
+create function cantidadDeCuotasPorPagar(@nroNotaDeVenta int)returns int
+as begin
+	declare @cantidadDeCuotasNoPagadas int
+	set @cantidadDeCuotasNoPagadas=(
+		select count(*)
+		from cuota
+		where cuota.nroNotaDeVenta=@nroNotaDeVenta and
+			cuota.estado=0)
+	declare @montoFaltante float
+	set @montoFaltante=@cantidadDeCuotasNoPagadas*(
+		select monto
+		from cuota 
+		where nroNotaDeVenta=@nroNotaDeVenta)
+	return @montoFaltante
+end
+
+/*4 Obtener el monto ganado
+	en un determinado mes*/
+create function montoGanado (@nroMes int)returns float
+as begin
+	declare @nroPagoDeCuota int
+	declare @montoGanado float
+	set @montoGanado=0
+	declare cursorNroPagoDeCuota cursor for(
+		select nro 
+		from pagoDeCuota
+		where month(fecha)=@nroMes)
+	open cursorNroPagoDeCuota
+	fetch from cursorNroPagoDeCuota into @nroPagoDeCuota
+	while(@@FETCH_STATUS=0)
+	begin
+		set @montoGanado=@montoGanado+(
+			select monto
+			from cuota,pagoDeCuota
+			where pagoDeCuota.nroCuota=cuota.nro and
+					pagoDeCuota.nro=@nroPagoDeCuota)
+		fetch from cursorNroPagoDeCuota into @nroPagoDeCuota
+	end
+	close cursorNroPagoDeCuota
+	deallocate cursorNroPagoDeCuota
+	return @montoGanado
+end
+select SIinegas.dbo.montoGanado(1)
+/*5 
 
 
 
